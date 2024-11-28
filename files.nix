80f8316f64faeea6e8dbc11a46f6156b335a730e
nohehf <nohe.hinniger.foray@gmail.com>
2024-11-28 12:13:43 +0100

    vscode settings symlink ok

--- files.nix
+++ files.nix
@@ -1,8 +1,9 @@
 { pkgs, config, ... }:
 let
   HOME = builtins.getEnv "HOME";
+  CONFIG = "${HOME}/config/config";
 in
 {
-  "${HOME}/Library/Application Support/Code/User/settings.json".source = config.lib.file.mkOutOfStoreSymlink ./config/vscode/settings.json;
-  "${HOME}/Library/Application Support/Code/User/keybindings.json".source = config.lib.file.mkOutOfStoreSymlink ./config/vscode/keybindings.json;
+  "${HOME}/Library/Application Support/Code/User/settings.json".source = config.lib.file.mkOutOfStoreSymlink "/Users/nohehf/config/config/vscode/settings.json";
+  "${HOME}/Library/Application Support/Code/User/keybindings.json".source = config.lib.file.mkOutOfStoreSymlink "${CONFIG}/vscode/keybindings.json";
 }
