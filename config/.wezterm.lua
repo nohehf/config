559f239722e0fc17cb2d6d16e6e8d58138cc69d6
nohehf <nohe.hinniger.foray@gmail.com>
2024-11-29 13:53:01 +0100

    wezterm config done

--- config/.wezterm.lua
+++ config/.wezterm.lua
@@ -6,9 +6,8 @@
 -- overrides.font_size = 18.0
 -- window:set_config_overrides(overrides)
 
-
 -- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
@@ -18,30 +17,34 @@ local act = wezterm.action
 
 -- For example, changing the color scheme:
 config.color_scheme = "Catppuccin Mocha"
-config.font = wezterm.font 'JetBrains Mono'
+config.font = wezterm.font("JetBrains Mono")
 config.window_background_opacity = 0.5
 config.macos_window_background_blur = 30
 
+-- hide config top bar
+config.window_decorations = "RESIZE"
+
+-- hide tabs if only one is present
+config.hide_tab_bar_if_only_one_tab = true
 
 config.keys = {
-    -- cmd + d to split horizontally
-    { key = "d",    mods = "CMD",       action = act { SplitHorizontal = { domain = "CurrentPaneDomain" } } },
-    -- cmd + shift + d to split vertically
-    { key = "d",    mods = "CMD|SHIFT", action = act { SplitVertical = { domain = "CurrentPaneDomain" } } },
-    -- close pane on cmd + w
-    { key = "w",    mods = "CMD",       action = act { CloseCurrentPane = { confirm = true } } },
+	-- cmd + d to split horizontally
+	{ key = "d", mods = "CMD", action = act({ SplitHorizontal = { domain = "CurrentPaneDomain" } }) },
+	-- cmd + shift + d to split vertically
+	{ key = "d", mods = "CMD|SHIFT", action = act({ SplitVertical = { domain = "CurrentPaneDomain" } }) },
+	-- close pane on cmd + w
+	{ key = "w", mods = "CMD", action = act({ CloseCurrentPane = { confirm = true } }) },
 
-    -- rebind to iterm defaults
-    -- opt letft/right to move by word
-    { mods = "OPT", key = "LeftArrow",  action = act.SendKey({ mods = "ALT", key = "b" }) },
-    { mods = "OPT", key = "RightArrow", action = act.SendKey({ mods = "ALT", key = "f" }) },
-    -- cmd left/right to move to start/end of line
-    { mods = "CMD", key = "LeftArrow",  action = act.SendKey({ mods = "CTRL", key = "a" }) },
-    { mods = "CMD", key = "RightArrow", action = act.SendKey({ mods = "CTRL", key = "e" }) },
-    -- cmd backspace to delete to start of line
-    { mods = "CMD", key = "Backspace",  action = act.SendKey({ mods = "CTRL", key = "u" }) },
+	-- rebind to iterm defaults
+	-- opt letft/right to move by word
+	{ mods = "OPT", key = "LeftArrow", action = act.SendKey({ mods = "ALT", key = "b" }) },
+	{ mods = "OPT", key = "RightArrow", action = act.SendKey({ mods = "ALT", key = "f" }) },
+	-- cmd left/right to move to start/end of line
+	{ mods = "CMD", key = "LeftArrow", action = act.SendKey({ mods = "CTRL", key = "a" }) },
+	{ mods = "CMD", key = "RightArrow", action = act.SendKey({ mods = "CTRL", key = "e" }) },
+	-- cmd backspace to delete to start of line
+	{ mods = "CMD", key = "Backspace", action = act.SendKey({ mods = "CTRL", key = "u" }) },
 }
-
 
 -- and finally, return the configuration to wezterm
 return config
